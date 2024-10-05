c3e23c237e9cf08678e995e542d28ff9afe27686
Yusup <yusuprd@icloud.com>
2024-10-06 04:30:17 +0800

    Added custom permissions file and installed django filters

--- books/admin.py
+++ books/admin.py
@@ -4,7 +4,8 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = '__all__'
+    list_display = ['id', 'title', 'author',
+                    'formatted_price', 'genre', 'publication_date']
     list_display_links = ['title']
     search_fields = ['title', 'author']
     list_filter = ['genre', 'publication_date']
--- books/views.py
+++ books/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from .permissions import IsAuthorOrReadOnly
+from django_filters import rest_framework as filters
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+class BookFilter(filters.FilterSet):
+    genre = filters.CharFilter(field_name="genre", lookup_expr="iexact")
+
+    class Meta:
+        model = Book
+        fields = ['genre']
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = BookFilter
+    pagination_class = PageNumberPagination
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def update(self, request, *args, **kwargs):
+        partial_data = {
+            "price": request.data.get("price", None),
+            "description": request.data.get("description", None),
+            "title": request.data.get("title", None),
+        }
+        return super().update(request, partial=partial_data, *args, **kwargs)
